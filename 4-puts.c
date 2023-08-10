64599da437094dfb5d31a7a2b1e7a8767bc99fe6
Yaseendesouky <abrahmdesouky107@gmail.com>
2023-08-09 23:52:03 -0700

    4-puts.c

--- 4-puts.c
+++ 4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main -Entry point 
  *
@@ -6,8 +7,9 @@
  *
  * Return: Alawys 0 (Success)
  */
+
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle"):
-	return (0):
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
