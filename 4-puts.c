c1c59f6bcd8b0e5327fd039272936435b61576e9
Yaseendesouky <abrahmdesouky107@gmail.com>
2023-08-09 23:47:20 -0700

    4-puts.c

---
+++ 4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -Entry point 
+ *
+ * Descripition: A C program that prints with put function 
+ *
+ * Return: Alawys 0 (Success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle"):
+	return (0):
+}
