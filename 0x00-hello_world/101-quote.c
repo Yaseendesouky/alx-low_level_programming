2bdd1874d5f40b58c7102cd9b72732f282c0c1f3
Yaseendesouky <abrahmdesouky107@gmail.com>
2023-08-10 20:54:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,14 +4,15 @@
  * main - Entry point
  *
  * Dsecription: print a qoute using write funcyion
- *      write(int fd, const void *buf, size_t count);
+ *    ssize-t write(int fd,const void *buf,size_t count);
  *
  * Return:1 (error).
 */
 
-int main(void)
-{       char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1 ,quo ,59);
-	return (1);
+int main(void)
+{char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+write(1, quo, 59);
+return (1);
 }
